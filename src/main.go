314d6e4f806c1b79c7ba7a9ce765f2d70e326692
David Bee <baeddavid@gmail.com>
2019-12-04 17:46:57 -0600

    update router

--- src/main.go
+++ src/main.go
@@ -13,6 +13,8 @@ func newRouter() *mux.Router {
 	staticFileDirectory := http.Dir("./assets/")
 	staticFileHandler := 	http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
 	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/birds", createBirdHandler).Methods("PUT")
 	return r
 }
 
